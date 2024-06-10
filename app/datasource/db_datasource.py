050db85b6f4d3dbd03588304c054c5eabde06ab0
Ardhy Satrio <rio27dh@gmail.com>
2024-06-10 10:57:52 +0000

    add error handler db connection

--- app/datasource/db_datasource.py
+++ app/datasource/db_datasource.py
@@ -1,10 +1,18 @@
+from sqlalchemy.exc import SQLAlchemyError
 from langchain_community.utilities import SQLDatabase
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 
 DB_URI = "mysql://uj1qnl4rlq8uzsk2:k8BXQj9UHy2OnC8B4GKy@botkgkqmgyoiovycsvln-mysql.services.clever-cloud.com:3306/botkgkqmgyoiovycsvln"
 
-db = SQLDatabase.from_uri(DB_URI)
+try:
+  db = SQLDatabase.from_uri(DB_URI)
+  print("Database connection successful")
+except SQLAlchemyError as e:
+  print(f"Failed to connect to the database: {e}")
+  db = None
 
 def get_toolkit(model):
-  toolkit = SQLDatabaseToolkit(db=db,llm=model)
+  if db is None:
+      raise RuntimeError("Database connection not available. Cannot create toolkit.")
+  toolkit = SQLDatabaseToolkit(db=db, llm=model)
   return toolkit
