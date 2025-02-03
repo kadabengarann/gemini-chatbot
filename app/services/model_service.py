343362d5f859ffffc42eb5639a766f94987fffa2
kadabengaran <rio27dh@gmail.com>
2025-02-03 09:43:03 +0800

    Integration with TogetherAI

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -4,14 +4,18 @@ from flask import current_app
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 IS_USING_GPT = None
 IS_USING_API = None
+IS_USING_EXTERNAL = None
 
 def generate_response(response, identifier, message_type=""):
     IS_USING_API = current_app.config['IS_USING_API']
     IS_USING_GPT = current_app.config['IS_USING_GPT']
+    IS_USING_EXTERNAL = current_app.config['IS_USING_EXTERNAL']
 
     if IS_USING_API:
       if IS_USING_GPT:
         from ..services.gpt_service_api  import generate_response as model_generate_response
+      elif IS_USING_EXTERNAL:
+        from ..services.external_service_api  import generate_response as model_generate_response
       else:
         from ..services.gemini_service_api  import generate_response as model_generate_response
     else:
